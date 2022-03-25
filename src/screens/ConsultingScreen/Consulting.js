8e36ec130513a439bae95bbc4378cf836d4be3e3
Amanda Choo Yun Xuan <amanda.choo.2018@smu.edu.sg>
2022-03-25 11:38:20 +0800

    update consulting.js
    added name

--- src/screens/ConsultingScreen/Consulting.js
+++ src/screens/ConsultingScreen/Consulting.js
@@ -1,14 +1,15 @@
 import React from 'react'
-
+//amanda
 import { useEffect, useState } from 'react';
 //for data import
 import * as Data from'../../api/data'
 import * as localData from '../../api/localHostData';
 import * as v9_firestore from '../../api/v9_firestore';
 import * as v9_auth from '../../api/v9_auth';
+import AccountBalance from '../../components/AccountBalance';
 const Consulting = () => {
   return (
-    <div>Consulting</div>
+   <AccountBalance></AccountBalance>
   )
 }
 
